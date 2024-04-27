656b93664a6dfda12e6137b7b28c4844327cd901
Khanieiev Ivan <75529688+iLynx26@users.noreply.github.com>
2024-04-27 12:41:14 -0400

    Update exercise_11.py

--- exercises/exercise_11.py
+++ exercises/exercise_11.py
@@ -1,1 +1,10 @@
-# Your solution to Exercise 11
+string = input()
+reversed_string = string[::-1]
+
+if " " in string:
+    indx = reversed_string.index(" ")
+    word = string[indx:]
+else:
+    word = string
+
+print(word)
