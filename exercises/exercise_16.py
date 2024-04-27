eb8ba71854ba6ee687f4ea3cdfb2340fd5d51166
Khanieiev Ivan <75529688+iLynx26@users.noreply.github.com>
2024-04-27 13:02:18 -0400

    Update exercise_16.py

--- exercises/exercise_16.py
+++ exercises/exercise_16.py
@@ -1,1 +1,12 @@
-# Your solution to Exercise 16
+char_1 = input()
+char_2 = input()
+
+output = ""
+
+for i in range(255):
+    if char_1 <= str(chr(i)) <= char_2:
+        output += str(chr(i))
+    elif chr(i) > char_2:
+        break
+    
+print(output)
