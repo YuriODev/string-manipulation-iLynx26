b0037369bfd47260d2fc8bb28162dec39ea2a67d
Khanieiev Ivan <75529688+iLynx26@users.noreply.github.com>
2024-05-11 11:46:45 -0400

    Update exercise_24.py

--- exercises/exercise_24.py
+++ exercises/exercise_24.py
@@ -1,1 +1,24 @@
-# Your solution to Exercise 24
+string = input()
+
+overall_characters = len(string)
+lowercase_characters = 0
+uppercase_characters = 0
+
+for char in string:
+    if char.upper() != char:
+        lowercase_characters += 1
+    elif char.lower() != char:
+        uppercase_characters += 1
+    else:
+        overall_characters -= 1
+
+
+if overall_characters != 0:
+    lowercase_percentage = lowercase_characters / overall_characters * 100
+    uppercase_percentage = uppercase_characters / overall_characters * 100
+
+    print(f"{lowercase_percentage:.2f}")
+    print(f"{uppercase_percentage:.2f}")
+else:
+    print("0.00")
+    print("0.00")
