e5115a3517be585dd7af2c6a775e73324f9f0368
Khanieiev Ivan <75529688+iLynx26@users.noreply.github.com>
2024-05-11 11:09:03 -0400

    Update exercise_22.py

--- exercises/exercise_22.py
+++ exercises/exercise_22.py
@@ -1,1 +1,10 @@
-# Your solution to Exercise 22
+string = input().lower()
+clean_string = ""
+for char in string:
+    if char.isdigit():
+        clean_string += char
+    elif 61 <= ord(char) <= 70:
+        clean_string += char
+
+answer = "Yes" if clean_string == clean_string[::-1] else "No"
+print(answer)
