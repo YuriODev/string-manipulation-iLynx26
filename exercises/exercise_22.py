f6a8358df883706d1d16d91998458445b3a78c4a
Khanieiev Ivan <75529688+iLynx26@users.noreply.github.com>
2024-05-18 11:31:40 -0400

    Update exercise_22.py

--- exercises/exercise_22.py
+++ exercises/exercise_22.py
@@ -1,10 +1,10 @@
-string = input().lower()
-clean_string = ""
-for char in string:
-    if char.isdigit():
-        clean_string += char
-    elif 61 <= ord(char) <= 70:
-        clean_string += char
+string = input().lower().replace(",", "").replace(" ", "")
+# clean_string = ""
+# for char in string:
+#     if char.isdigit():
+#         clean_string += char
+#     elif 61 <= ord(char) <= 70:
+#         clean_string += char
 
-answer = "Yes" if clean_string == clean_string[::-1] else "No"
+answer = "Yes" if string == string[::-1] else "No"
 print(answer)
