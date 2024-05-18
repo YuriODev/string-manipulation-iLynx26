2f2fa09573c9d05946cf74222e3ab66e276faf27
Khanieiev Ivan <75529688+iLynx26@users.noreply.github.com>
2024-05-18 11:02:54 -0400

    Update exercise_26.py

--- exercises/exercise_26.py
+++ exercises/exercise_26.py
@@ -1,1 +1,15 @@
-# Your solution to Exercise 26
+collumn_1 = int(input())
+collumn_2 = int(input())+1
+row_1 = int(input())
+row_2 = int(input())
+
+output_1 = "\t"
+for i in range(row_1, row_2+1):
+    output_1 += f"{i}\t"
+
+print(output_1)
+for i in range(collumn_1, collumn_2):
+    output = f"{i}\t"
+    for j in range(row_1, row_2+1):
+        output += f"{j*i}\t"
+    print(output)
